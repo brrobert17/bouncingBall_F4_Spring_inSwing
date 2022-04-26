09e8d1effc55967f4b1ad7fd99498055cbae1aea
brrobert17 <73032043+brrobert17@users.noreply.github.com>
2022-04-26 13:36:32 +0200

    Initial commit

---
+++ src/main/java/com/example/bouncingball_f4_spring_inswing/BouncingBallF4SpringInSwingApplication.java
@@ -0,0 +1,13 @@
+package com.example.bouncingball_f4_spring_inswing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouncingBallF4SpringInSwingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BouncingBallF4SpringInSwingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bouncingball_f4_spring_inswing/BouncingBallF4SpringInSwingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bouncingball_f4_spring_inswing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouncingBallF4SpringInSwingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
