f688606fb8fc581a8bd929126b6f523942183f3f
brrobert17 <73032043+brrobert17@users.noreply.github.com>
2022-04-27 13:23:02 +0200

    initR2

--- src/main/java/com/example/bouncingball_f4_spring_inswing/controller/HomeController.java
+++ src/main/java/com/example/bouncingball_f4_spring_inswing/controller/HomeController.java
@@ -3,12 +3,14 @@ package com.example.bouncingball_f4_spring_inswing.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @GetMapping
-    public String index(Model model) {
-        return "home/index";
+    @RequestMapping
+    public String helloWorld() {
+        return "Hello World!";
     }
 }
