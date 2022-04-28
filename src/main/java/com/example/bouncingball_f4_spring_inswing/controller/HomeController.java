95fb5c33371a6c453a12963416aece9516c7ff3e
Arkitekt-s <sfattahieh@gmail.com>
2022-04-28 09:19:38 +0200

    new video

--- src/main/java/com/example/bouncingball_f4_spring_inswing/controller/HomeController.java
+++ src/main/java/com/example/bouncingball_f4_spring_inswing/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.example.bouncingball_f4_spring_inswing.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 @RestController
 public class HomeController {
@@ -10,5 +16,22 @@ public class HomeController {
     public String index() {
         return ("templates/index.html");
     }
-
+   //downlowd video file from template
+    @GetMapping("/download_file")
+    public void download(HttpServletResponse response) throws IOException {
+        File file = new File("src/main/resources/templates/2022-04-27 15-27-29.mp4");
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
+        response.setContentLength((int) file.length());
+        InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+        FileCopyUtils.copy(inputStream, response.getOutputStream());
+    }
 }
+
+
+
+
+
+
+
+
